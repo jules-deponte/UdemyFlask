3419fbcb98cbcd197391ea1f3e9c44295b9c4fd1
Jules de Ponte <julesdeponte@gmail.com>
2020-06-11 18:30:12 +0200

    in Keyword

--- AdvancedSetOperations/Udemy Flask.py
+++ AdvancedSetOperations/Udemy Flask.py
@@ -1,5 +1,14 @@
 friends = {'alice', 'bob', 'charlie'}
-abroad = {'alice', 'bob'}
+abroad = {'alice', 'bob', 'Doug'}
 
 local_friends = friends.difference(abroad)
 print(local_friends)
+
+all_friends = friends.union(abroad)
+print(all_friends)
+
+art = {'alice', 'bob', 'cahrlie', 'doug'}
+science = {'Earnest', 'frank', 'alice', 'bob'}
+
+both_subjects = art.intersection(science)
+print(both_subjects)
