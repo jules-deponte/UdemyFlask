028a4b4759e166e13e665fdc883cca5ab313c3c6
Jules de Ponte <julesdeponte@gmail.com>
2020-06-17 08:56:56 +0200

    SQLite

---
+++ Flask/Section 5/test.py
@@ -0,0 +1,28 @@
+import sqlite3
+
+connection = sqlite3.connect('data.db')
+
+cursor = connection.cursor()
+
+create_table = "CREATE TABLE users (ID int, username text, password text)"
+cursor.execute(create_table)
+
+user = (1, 'bob', 'asdf')
+
+insert_query = "INSERT INTO users VALUES (?, ?, ?)"
+cursor.execute(insert_query, user)
+
+users = [
+    (2, 'alice', 'qwerty'),
+    (3, 'charlie', 'zxcv')
+]
+
+cursor.executemany(insert_query, users)
+
+select_qurery = "SELECT * FROM users"
+for row in cursor.execute(select_qurery):
+    print(row)
+
+connection.commit()
+
+connection.close()
